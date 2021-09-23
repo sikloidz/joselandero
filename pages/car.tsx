e8c8c43987fa3ecc8e066705fce4458cc0ae2c40
Jose Landero <jose.landero@meru.mx>
2021-09-23 18:48:44 -0500

    Confetti!

--- pages/car.tsx
+++ pages/car.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { isAfter, parseISO, intervalToDuration } from 'date-fns';
+import Confetti from 'react-confetti';
 
 const targetTime = parseISO('2021-09-24T11:00:00-05:00');
 
 export default function Car() {
+  const [showConfetti, setShowConfetti] = useState(false);
   const dateNode = useRef<HTMLHeadingElement>();
 
   useEffect(() => {
@@ -39,22 +41,33 @@ export default function Car() {
   });
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <div>
-        <h2 className="text-white text-opacity-75 text-center pb-4">
-          🙀 El carrito llega en
-        </h2>
-        <h1
-          ref={dateNode}
-          className="text-white text-6xl font-bold text-center"
-        >
-          {[
-            duration.hours.toString().padStart(2, '0'),
-            duration.minutes.toString().padStart(2, '0'),
-            duration.seconds.toString().padStart(2, '0'),
-          ].join(':')}
-        </h1>
+    <>
+      {showConfetti && <Confetti gravity={0.2} />}
+      <div className="w-screen h-screen flex items-center justify-center">
+        <div className="text-center">
+          <h2 className="text-white text-opacity-75 text-center pb-4">
+            🙀 El carrito llega en
+          </h2>
+          <h1
+            ref={dateNode}
+            className="text-white text-6xl font-bold text-center"
+          >
+            {[
+              duration.hours.toString().padStart(2, '0'),
+              duration.minutes.toString().padStart(2, '0'),
+              duration.seconds.toString().padStart(2, '0'),
+            ].join(':')}
+          </h1>
+          <div className="pt-8">
+            <button
+              className="bg-gray-100 rounded-md py-4 px-6 text-gray-800 text-2xl font-bold border-2 border-gray-200"
+              onClick={() => setShowConfetti(true)}
+            >
+              Yay! 🥳
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
